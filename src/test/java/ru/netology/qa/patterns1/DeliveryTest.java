1d2053dc4e805175693246e5d9854490e4f624f5
Амир Байтулинов <amiricus@gmail.com>
2022-08-23 19:07:57 +0500

    add new test

---
+++ src/test/java/ru/netology/qa/patterns1/DeliveryTest.java
@@ -0,0 +1,50 @@
+package ru.netology.qa.patterns1;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[placeholder='Город']").setValue(validUser.getCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").sendKeys(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(By.className("button")).click();
+        $(By.className("notification_visible")).shouldBe(visible, Duration.ofSeconds(30));
+        $(By.className("notification__title")).shouldHave(exactText("Успешно!"));
+        $(By.className("notification__content")).shouldHave(exactText("Встреча успешно забронирована на " + firstMeetingDate));
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").sendKeys(secondMeetingDate);
+        $(By.className("button")).click();
+        $(By.className("notification_visible")).shouldBe(visible, Duration.ofSeconds(30));
+        $(By.className("notification__title")).shouldHave(exactText("Успешно!"));
+        $(By.className("notification__content")).shouldHave(exactText("Встреча успешно забронирована на " + secondMeetingDate));
+    }
+}
