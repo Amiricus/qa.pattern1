d4d99ce7845f3adf217a146f3e924b18261f9772
Амир Байтулинов <amiricus@gmail.com>
2022-08-31 20:03:40 +0500

    add new cssSelector

--- src/test/java/ru/netology/qa/patterns1/DeliveryTest.java
+++ src/test/java/ru/netology/qa/patterns1/DeliveryTest.java
@@ -43,7 +43,7 @@ class DeliveryTest {
         $("[placeholder='Дата встречи']").sendKeys(secondMeetingDate);
         $(By.className("button")).click();
         $("[data-test-id=replan-notification] .notification__title").shouldHave(exactText("Необходимо подтверждение"));
-        $(By.className("button__text")).click();
+        $("[data-test-id=replan-notification] .button").click();
         $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
